cd99b678106f7b596138ba7a3587c31c168b2e82
AMAN LADWA <ladwaaman@gmail.com>
2021-04-27 13:22:25 +0530

    Completed simple binary search

--- Exercise_1.java
+++ Exercise_1.java
@@ -3,6 +3,16 @@ class BinarySearch {
     int binarySearch(int arr[], int l, int r, int x) 
     { 
         //Write your code here
+        while(l<=h){
+            int mid = l+(h-l)/2;
+            if(arr[mid]==x) return mid;
+            if(arr[mid]<x){
+                high = mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return -1
     } 
   
     // Driver method to test above 
